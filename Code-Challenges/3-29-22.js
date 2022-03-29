2c2713d1643a8d634d7e61948dae244b30ae2eed
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-03-29 18:46:00 -0400

    add code challenge 3-29-22

---
+++ Code-Challenges/3-29-22.js
@@ -0,0 +1,10 @@
+// Name: validate code with simple regex
+
+// Instructions:
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+
+// You can assume the input will always be a number.
+
+// Answer:
+
+const validateCode = code => /^[1-3]/g.test(code)
