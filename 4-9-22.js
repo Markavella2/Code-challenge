cfa6802a8787658027aa0b57462d9e640c834040
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-04-09 21:36:01 -0400

    add code challenge 4-9-22

---
+++ 4-9-22.js
@@ -0,0 +1,15 @@
+// Name: Convert a Number to a String!
+
+// Instructions:
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+// Answer:
+function numberToString(num) {
+    return num.toString()
+  }
