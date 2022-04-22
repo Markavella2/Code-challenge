ab1e4378a6c61dbd3a5bf69a009667cddc92e551
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-04-21 22:15:08 -0400

    add code challenge 4-21-22

---
+++ 4-21-22.js
@@ -0,0 +1,16 @@
+// Name: Covert a Boolean to string
+
+// INstructions:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// Answer:
+function booleanToString(b){
+    //your code here
+    if (b==true){
+      return "true"
+    }else {
+      return "false"
+    }
+  }
