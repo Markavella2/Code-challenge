2b1a0938dc3761acc323437db6e601169da061d5
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-04-26 20:44:38 -0400

    Add code challenge 4-26-22

---
+++ 4-26-22.js
@@ -0,0 +1,12 @@
+// Name: Name Shuffler
+
+// Instructions:
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+
+// Answer:
+function nameShuffler(str){
+    return str.split(" ").reverse().join(" ")
+  }
