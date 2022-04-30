7d652eac3216a821e0c6673d142548cd9e33d69a
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-04-29 23:29:48 -0400

    add code challenge 4-29-22

---
+++ 4-29-22.js
@@ -0,0 +1,12 @@
+// Name: Is there a vowel in there?
+
+// INstructions:
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+// Answer:
+
+isVow = a => a.map(x => /[aeiou]/.test(String.fromCharCode(x)) ? String.fromCharCode(x) : x)
