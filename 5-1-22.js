614863f274664eab88182cb36cbfd5934e53a502
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-01 23:10:11 -0400

    code challenge 5-1-22

---
+++ 5-1-22.js
@@ -0,0 +1,12 @@
+// Name: Sum Mixed Array
+
+// Instructions:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+// Answer:
+function sumMix(x){
+    return x.reduce((a,c) => a + Number(c) , 0)
+    }
+    
