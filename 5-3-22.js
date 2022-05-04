333d000f16955f82f8faa533d1630f17ee7bf0f5
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-03 22:31:18 -0400

    add another code challenge 5-3-22

--- 5-3-22.js
+++ 5-3-22.js
@@ -17,20 +17,41 @@
 
 
 // Answer:
-function getDrinkByProfession(param){
-    if (param.toLowerCase() === 'jabroni'){
-        return "Patron Tequila"
-      }else if (param.toLowerCase() == "school counselor"){
-        return "Anything with Alcohol"
-      } else if (param.toLowerCase() == "programmer"){
-        return "Hipster Craft Beer"
-      } else if (param.toLowerCase() == "bike gang member"){
-        return "Moonshine"
-      } else if (param.toLowerCase() == "politician"){
-        return "Your tax dollars"
-      } else if (param.toLowerCase() == "rapper"){
-        return "Cristal"
-      } else{
-        return "Beer" 
-      } 
+// function getDrinkByProfession(param){
+//     if (param.toLowerCase() === 'jabroni'){
+//         return "Patron Tequila"
+//       }else if (param.toLowerCase() == "school counselor"){
+//         return "Anything with Alcohol"
+//       } else if (param.toLowerCase() == "programmer"){
+//         return "Hipster Craft Beer"
+//       } else if (param.toLowerCase() == "bike gang member"){
+//         return "Moonshine"
+//       } else if (param.toLowerCase() == "politician"){
+//         return "Your tax dollars"
+//       } else if (param.toLowerCase() == "rapper"){
+//         return "Cristal"
+//       } else{
+//         return "Beer" 
+//       } 
+//   }
+
+
+
+
+// Name: Plural
+
+// Instructions:
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
+
+// Answer:
+function plural(n) {
+    if (n === 1){
+      return false
+    }else{
+      return true 
+    }
   }
