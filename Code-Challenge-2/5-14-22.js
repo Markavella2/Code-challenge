3a6e5d267961f03bc7b12708bef70587f725e17b
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-14 23:29:26 -0400

    add another code challenge 5-14-22

--- Code-Challenge-2/5-14-22.js
+++ Code-Challenge-2/5-14-22.js
@@ -31,11 +31,41 @@
 //   Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
 
 //   Answer:
-  const firstLetter = str => str[0].toUpperCase()
+//   const firstLetter = str => str[0].toUpperCase()
 
-const isValidName = name => /[a-z]/gi.test(name)
+// const isValidName = name => /[a-z]/gi.test(name)
 
-const aliasGen = (fName, lName) =>
-  isValidName(fName[0]) && isValidName(lName[0])
-    ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(lName)]}`
-    : 'Your name must start with a letter from A - Z.'
+// const aliasGen = (fName, lName) =>
+//   isValidName(fName[0]) && isValidName(lName[0])
+//     ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(lName)]}`
+//     : 'Your name must start with a letter from A - Z.'
+
+
+// Name:
+// Count WOrds
+
+// Instructions:
+// Can you implement a function that will return number of words in a string?
+
+// You have to ensure that spaces in string is a whitespace for real.
+
+// Let's take a look on some examples:
+
+// countWords("Hello"); // returns 1 as int
+// countWords("Hello, World!") // returns 2
+// countWords("No results for search term `s`") // returns 6
+// countWords(" Hello") // returns 1
+// // ... and so on
+// What kind of tests we made for your code:
+
+// Function have to count words and not spaces. You have to be sure that you doing it right
+// Empty string has no words.
+// String with spaces around should be trimmed.
+// Non-whitespace (ex. breakspace, unicode chars) should be treated as a delimiter
+// Doublecheck that words with chars like -, ', ` are counted right.
+
+
+// Answer:
+function countWords(str) {
+return str.split(/\s/g).filter(Boolean).length
+}
