c9b55c00bad5d6287f278d30b8c7b2604d487e67
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-17 21:53:21 -0400

    add another code challenge 5-17-22

--- Code-Challenge-2/5-17-22.js
+++ Code-Challenge-2/5-17-22.js
@@ -6,4 +6,31 @@
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 // Answer:
-const squareSum = numbers => numbers.reduce((a,c) => a + c**2, 0)
+// const squareSum = numbers => numbers.reduce((a,c) => a + c**2, 0)
+
+
+// Code Challenge 2
+
+// Name: Count of positives/sum of negatives
+
+// Instructions:
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// Answer:
+function countPositivesSumNegatives(input) {
+    let x = 0;
+    let y = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? x++ : y += num);
+    }
+    return [x , y];
+}
