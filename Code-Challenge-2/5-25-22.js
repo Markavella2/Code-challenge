db874f33231345449bd7c614f46e5afff246ae04
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-25 16:30:15 -0400

    add code challenge 5-25-22

---
+++ Code-Challenge-2/5-25-22.js
@@ -0,0 +1,11 @@
+// Name:Beginner - Reduce but Grow
+
+// Instructions:Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// Answer:
+function grow(x){
+    const answer = x.reduce((a,c) => a*c,1)
+    return answer
+    }
