8fb3b4f42d67c1c2a5746fae45d1fb3c046bf84c
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-05-26 14:46:16 -0400

    add code challenge 5-26-22

---
+++ Code-Challenge-2/5-26-22.js
@@ -0,0 +1,20 @@
+// Name:Swap Values
+
+// Instructions:
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+
+
+
+// Their wrong code:
+// function swapValues() {
+//     var args = Array.prototype.slice.call(arguments);
+//     var temp = args[0];
+//     args[0] = args[1];
+//     args[1] = temp;
+// }
+
+
+// Answer:
+const swapValues = arr => arr.reverse()
