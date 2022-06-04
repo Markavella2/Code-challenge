8c327a61e5ce390d705edfed6394b33d80231127
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-03 21:56:32 -0400

    add code challenge 6-3-22

---
+++ Code-Challenge-2/6-3-22.js
@@ -0,0 +1,15 @@
+// Name: Printing Array elements with Comma delimiters
+
+// Instructions:
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Answer:
+function printArray(array){
+    return array.toString()
+  }
