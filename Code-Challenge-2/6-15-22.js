c71f2a99e2eef02bf53aa4e52908e1ab91ae1f22
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-15 18:27:18 -0400

    add code challenge 6-15-22

---
+++ Code-Challenge-2/6-15-22.js
@@ -0,0 +1,10 @@
+// Name:
+// You Can't Code Under Pressure #1
+
+// Instructions:
+// Code as fast as you can! You need to double the integer and return it.
+
+// Answer:
+function doubleInteger(i) {
+    return i*2;
+  }
