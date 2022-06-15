912829625a040483be3e4e998f8894cadb7e4e1a
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-14 21:16:58 -0400

    add refactored code challenge 6-14-22

--- Code-Challenge-2/6-14-22.js
+++ Code-Challenge-2/6-14-22.js
@@ -6,10 +6,16 @@
 
 // Answer:
 
+// function simpleMultiplication(number) {
+//     if (number % 2 == 0){
+//       return number*8
+//     }else{
+//       return number*9
+//     }
+// }
+
+//REFACTORED ANSWER:
+
 function simpleMultiplication(number) {
-    if (number % 2 == 0){
-      return number*8
-    }else{
-      return number*9
-    }
-}
+    return  number % 2 === 0 ? number*8 : number*9
+ }
