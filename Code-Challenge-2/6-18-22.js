d7969e810ed59a18e85f9f99dc09edbd69f8ce02
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-18 22:27:17 -0400

    second code challenge 6-18-22

--- Code-Challenge-2/6-18-22.js
+++ Code-Challenge-2/6-18-22.js
@@ -33,12 +33,40 @@
 
 // Answer:
 
-class Ship{
-    constructor(draft, crew){
-      this.draft = draft;
-      this.crew = crew;
+// class Ship{
+//     constructor(draft, crew){
+//       this.draft = draft;
+//       this.crew = crew;
+//     }
+//     isWorthIt(){
+//       return this.draft - (this.crew*1.5) > 20
+//     }
+//   }
+
+  
+//   Second CodeWar:
+
+//   Name:Difference of Volumes of Cuboids
+
+
+//   Instructions:
+//   In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code.
+
+//   Answer:
+
+  function findDifference(a, b) {
+    let aa = a.reduce((a,c) => a*c,1)
+    let bb = b.reduce((a,c) => a*c,1)
+    let c = aa - bb
+    if (c < 0){
+      return c*-1
     }
-    isWorthIt(){
-      return this.draft - (this.crew*1.5) > 20
-    }
+    return c
   }
+  
