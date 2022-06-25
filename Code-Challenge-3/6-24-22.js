cf5e8c7eff48c1cbf07475bc36447f2fa7579556
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-24 22:49:44 -0400

    adding third codewar 6-24-22

--- Code-Challenge-3/6-24-22.js
+++ Code-Challenge-3/6-24-22.js
@@ -55,6 +55,31 @@
 
 //   Answer:
 
-  function eachCons(array, n) {
-    return array.map((x,y) => array.slice(y, y+n)).filter((x => x.length == n))
-    }
+//   function eachCons(array, n) {
+//     return array.map((x,y) => array.slice(y, y+n)).filter((x => x.length == n))
+//     }
+
+
+//     THIRD CODEWAR
+
+//     Name: Is it even?
+
+//     Instructions:
+//     In this Kata we are passing a number (n) into a function.
+
+//     Your code will determine if the number passed is even (or not).
+    
+//     The function needs to return either a true or false.
+    
+//     Numbers may be positive or negative, integers or floats.
+    
+//     Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+//     Answer:
+    function testEven(n) {
+        if (n%2==0){
+          return true
+        }else{
+          return false
+        }
+     }
