56d59d1f7c6b1ce4561eb8b254af526a2837d94f
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-06-29 14:37:15 -0400

    add refactored code challenge answer 6-29-22

--- Code-Challenge-3/6-29-22.js
+++ Code-Challenge-3/6-29-22.js
@@ -5,6 +5,11 @@
 // Complete the function which converts hex number (given as a string) to a decimal number.
 
 // Answer:
-function hexToDec(hexString){
-    return parseInt(hexString, 16)
-  }
+// function hexToDec(hexString){
+//     return parseInt(hexString, 16)
+//   }
+
+
+//   REFACTORED ANSWER:
+
+  const hexToDec = hexString => parseInt(hexString, 16)
