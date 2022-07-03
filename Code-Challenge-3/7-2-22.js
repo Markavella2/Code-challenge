dac6e10da75181cead3ac42fc04290ebeff56073
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-07-02 20:13:19 -0400

    add third code challenge 7-2-22

--- Code-Challenge-3/7-2-22.js
+++ Code-Challenge-3/7-2-22.js
@@ -65,14 +65,32 @@
 //   20, 2 --> 0
 
 //   Answer:
-  function finalGrade (exam, projects) {
-    if ((exam > 90) || (projects > 10)){
-      return 100
-    }else if ((exam > 75) && (projects >= 5)){
-      return 90
-    }else if ((exam > 50) && (projects >= 2)){
-      return 75
-    }else{
-      return 0
-  }
-    }
+//   function finalGrade (exam, projects) {
+//     if ((exam > 90) || (projects > 10)){
+//       return 100
+//     }else if ((exam > 75) && (projects >= 5)){
+//       return 90
+//     }else if ((exam > 50) && (projects >= 2)){
+//       return 75
+//     }else{
+//       return 0
+//   }
+//     }
+
+//     THIRD CODEWAR
+
+//     Name:Is he gonna survive?
+
+//     Instructions:
+//     A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+//     Return True if yes, False otherwise :)
+
+//     Answer:
+    function hero(bullets, dragons){
+        if (bullets/2 >= dragons){
+          return true
+        }else{
+          return false
+        }
+       }
