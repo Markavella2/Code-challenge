8b436e86af8cbf6c4d4c99576dc087f5a6acb539
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-07-08 18:36:43 -0400

    adding code challenge 7-8-22

---
+++ Code-Challenge-3/7-8-22.js
@@ -0,0 +1,11 @@
+// Name: Find Nearest Square Number
+// 
+//Instructions:
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+// Goodluck :)
+//
+
+//Answer:
+function nearestSq(n){
+    return Math.round(n**0.5)**2
+}
