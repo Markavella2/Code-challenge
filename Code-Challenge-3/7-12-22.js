d7383845098c95206a6e52e6b914c5fa2e6d8b55
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-07-12 14:47:04 -0400

    adding refactored answer to code challenge 7-12-22

--- Code-Challenge-3/7-12-22.js
+++ Code-Challenge-3/7-12-22.js
@@ -10,15 +10,33 @@
 
 // Answer:
 
+// const switchItUp = number => {
+//     if (number == 0) return "Zero";
+//     if (number == 1) return "One";
+//     if (number == 2) return "Two";
+//     if (number == 3) return "Three";
+//     if (number == 4) return "Four";
+//     if (number == 5) return "Five";
+//     if (number == 6) return "Six"; 
+//     if (number == 7) return "Seven";
+//     if (number == 8) return "Eight";
+//     if (number == 9) return "Nine";
+//   }
+
+
+//   REFACTORED ANSWER:
+
 const switchItUp = number => {
-    if (number == 0) return "Zero";
-    if (number == 1) return "One";
-    if (number == 2) return "Two";
-    if (number == 3) return "Three";
-    if (number == 4) return "Four";
-    if (number == 5) return "Five";
-    if (number == 6) return "Six"; 
-    if (number == 7) return "Seven";
-    if (number == 8) return "Eight";
-    if (number == 9) return "Nine";
+    switch (number){
+    case 0: return "Zero";
+    case 1: return "One";
+    case 2: return "Two"
+    case 3: return "Three";
+    case 4: return "Four";
+    case 5: return "Five"
+    case 6: return "Six";
+    case 7: return "Seven";
+    case 8: return "Eight";
+    case 9: return "Nine"
   }
+    }
