d3fc56454a8ea8f9baf8a7efb728bae846af9d98
MintyFreshBro <102497017+MintyFreshBro@users.noreply.github.com>
2022-07-15 15:36:20 -0400

    adding code challenge 7-15-22

---
+++ Code-Challenge-3/7-15-22.js
@@ -0,0 +1,44 @@
+// Name:Find the position!
+
+// Instructions:
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+// Answer:
+function position(letter){
+    const pos = "Position of alphabet: "
+  switch (letter){
+    case 'a': return `${pos}1`;
+    case 'b': return `${pos}2`;
+    case 'c': return `${pos}3`;
+    case 'd': return `${pos}4`;
+    case 'e': return `${pos}5`;
+    case 'f': return `${pos}6`;
+    case 'g': return `${pos}7`;
+    case 'h': return `${pos}8`;
+    case 'i': return `${pos}9`;
+    case 'j': return `${pos}10`;
+    case 'k': return `${pos}11`;
+    case 'l': return `${pos}12`;
+    case 'm': return `${pos}13`;
+    case 'n': return `${pos}14`;
+    case 'o': return `${pos}15`;
+    case 'p': return `${pos}16`;
+    case 'q': return `${pos}17`;
+    case 'r': return `${pos}18`;
+    case 's': return `${pos}19`;
+    case 't': return `${pos}20`;
+    case 'u': return `${pos}21`;
+    case 'v': return `${pos}22`;
+    case 'w': return `${pos}23`;
+    case 'x': return `${pos}24`;
+    case 'y': return `${pos}25`;
+    case 'z': return `${pos}26`;
+  }
+  }
+  
+  
+  
